d5264047e409e682e9bdacfb6915603ca244b6d5
badri-s <56728283+badri-s@users.noreply.github.com>
2020-01-11 16:24:31 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        getStart.setOnClickListener {
+            logLinerLay.setVisibility(View.VISIBLE)
+            getStart.setVisibility(View.INVISIBLE)
+            val loginai = AnimationUtils.loadAnimation(this, R.anim.log_in)
+            val mtavarlay = findViewById(R.id.logLinerLay) as FrameLayout
+            mtavarlay.startAnimation(loginai)
+        }
+        facebookBtt.setOnClickListener{
+            facebookLay.setVisibility(View.VISIBLE)
+            frameMain.setVisibility(View.VISIBLE)
+        }
+        googleBtt.setOnClickListener {
+            googleLay.setVisibility(View.VISIBLE)
+            frameMain.setVisibility(View.VISIBLE)
+        }
+        frameMain.setOnClickListener {
+            facebookLay.setVisibility(View.INVISIBLE)
+            googleLay.setVisibility(View.INVISIBLE)
+            frameMain.setVisibility(View.INVISIBLE)
+        }
+        registrartion.setOnClickListener {
+            logInLay.setVisibility(View.INVISIBLE)
+            forgotReg.setVisibility(View.INVISIBLE)
+            signUpLay.setVisibility(View.VISIBLE)
+            backToLogIn.setVisibility(View.VISIBLE)
+        }
+        backToLogIn.setOnClickListener {
+            logInLay.setVisibility(View.VISIBLE)
+            forgotReg.setVisibility(View.VISIBLE)
+            signUpLay.setVisibility(View.INVISIBLE)
+            backToLogIn.setVisibility(View.INVISIBLE)
+        }
+        logIn.setOnClickListener {
+            auth.createUserWithEmailAndPassword(emailIn.text.toString(), passwordIn.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful)
+                        Toast.makeText(this, "Successful register!", Toast.LENGTH_LONG).show()
+                    else
+                        Toast.makeText(this, "ar shesrulda", Toast.LENGTH_LONG).show()
+                }
+        }
+        auth.createUserWithEmailAndPassword(emailUp.text.toString(), passwordUp.text.toString()).addOnCompleteListener{
+            if(it.isSuccessful)
+            {
+                logInLay.setVisibility(View.VISIBLE)
+                forgotReg.setVisibility(View.VISIBLE)
+                signUpLay.setVisibility(View.INVISIBLE)
+                backToLogIn.setVisibility(View.INVISIBLE)
+            }
+        }
+
+    }
+
+
+
+
+
+
+}
